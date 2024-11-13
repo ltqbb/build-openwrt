142c93588b8b14b8760475fbb80797afa5fd6d75
ltqbb <125167546+ltqbb@users.noreply.github.com>
2024-11-13 13:55:56 +0800

    Update diy-23.05.sh

--- diy-23.05.sh
+++ diy-23.05.sh
@@ -90,3 +90,10 @@ sed -i 's/services\///g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-st
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
